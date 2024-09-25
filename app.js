20bcb2f9f7c7ce55d740419b35cd9966442c85fe
calderon104 <gonza.rc1004@gmail.com>
2024-09-25 14:26:48 -0300

    .

--- app.js
+++ app.js
@@ -83,19 +83,28 @@ app.get("/", (req, res) => {
 
 
 app.post('/translate', (req, res) => {
+  console.log("en el post de traducir");
+  console.log(req.body); // Verifica qué datos llegan
   const { text, targetLang } = req.body;
 
+  if (!text || !targetLang) {
+      return res.status(400).json({ error: 'Faltan parámetros de texto o idioma.' });
+  }
+
   translate({
-      text: text,
-      source: 'en', // Idioma de origen (Inglés)
-      target: targetLang, // Idioma de destino
-  }, (result) => {
-      if (result && result.translation) {
-          res.json({ translatedText: result.translation });
-      } else {
-          res.status(500).json({ error: 'Error al traducir el texto' });
-      }
-  });
+    text: text,
+    source: 'en',
+    target: targetLang,
+}, (result) => {
+  
+    console.log("el resultado de la traduccion es: "+result); // Muestra el resultado de la traducción para depuración
+    if (result && result.translation) {
+        res.json({ translatedText: result.translation });
+    } else {
+        res.status(500).json({ error: 'Error al traducir el texto' });
+    }
+});
+
 });
 app.use((req, res, next) => {
   res.status(404).send("No se encontro su pagina :( ");
